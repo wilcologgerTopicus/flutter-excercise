11928f65e470f6463cb55ed01658724e242414bc
wilco.logger <wilco.logger@topicus.nl>
2023-06-27 14:54:09 +0200

    make dice roll random number

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/styled_text.dart';
+import 'package:flutter_basic/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -12,8 +12,6 @@ class GradientContainer extends StatelessWidget {
     required this.colors,
   });
 
-  void rollDice() {}
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,24 +22,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(8.0),
-                foregroundColor: Colors.white,
-              ),
-              child: const StyledText('text'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
